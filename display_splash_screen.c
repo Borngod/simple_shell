f77edf74cbff5b20244ad73e71fcc3316c8b0454
Ayivi Angela <104152275+bloominghowl@users.noreply.github.com>
2023-02-22 17:46:03 +0000

    Update display_splash_screen.c

--- display_splash_screen.c
+++ display_splash_screen.c
@@ -5,7 +5,6 @@
  * display_splash_screen - displays custom ascii art when program starts.
  * @file_ptr - pointer to .txt file that contains ascii art
  */
-
 void display_splash_screen(FILE *file_ptr)
 {
 	char read_string[MAX_LEN];
